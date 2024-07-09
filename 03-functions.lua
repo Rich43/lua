04e223c33469cea359f10ff532ee75e6dbc15744
Richard Ward <Richard.Ward637@Outlook.com>
2024-07-09 19:16:02 +0100

    Add functions

---
+++ 03-functions.lua
@@ -0,0 +1,14 @@
+print("--- Functions ---")
+
+-- Function definition
+local function add(a, b)
+    return a + b
+end
+
+local function greet(name)
+    return "Hello, " .. name
+end
+
+-- Calling functions
+print(add(10, 5))         -- Output: 15
+print(greet("Alice"))     -- Output: Hello, Alice
