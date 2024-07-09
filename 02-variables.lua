cb2741268b6ab2870661bd1e87e87630d8fa8162
Richard Ward <Richard.Ward637@Outlook.com>
2024-07-09 19:13:07 +0100

    Improve variables

--- 02-variables.lua
+++ 02-variables.lua
@@ -1,9 +1,24 @@
+print("--- Integers ---")
 local W, H = 150, 375 -- window dimensions
 print(W)
 print(H)
 
+print("--- Strings ---")
+
 local testStr = "Hello World"
 print(testStr)
 
+print("--- Lists ---")
+
 local list = {"A", "B", "C", "D"}
-print(list[0])
+--- In most languages arrays start at 0, but in lua, 1 is the first element
+print(list[1])
+print(list[4])
+
+print("--- Dictionaries ---")
+
+local dictionary = {A = "E", B = "F", C = "G", D = "H"}
+print(dictionary["B"])
+print(dictionary["D"])
+dictionary["sausages"] = "eggs"
+print(dictionary["sausages"])
